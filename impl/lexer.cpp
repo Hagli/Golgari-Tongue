6825f6f45c630d62fc17b4aa4aa9976848c416d8
Hagli <hagli2001@gmail.com>
2022-07-26 14:09:27 +0700

    Add lexer and parser in a new folder.

---
+++ impl/lexer.cpp
@@ -0,0 +1,63 @@
+//Taken from the tutorial at
+//https://llvm.org/docs/tutorial/MyFirstLanguageFrontend/index.html
+#include "../header/lexer.h"
+
+//reminder to change this from global variable
+//static std::string IdentifierStr; Filled in if tok_identifier
+//static int NumVal;              Filled in if tok_number
+
+static int gettok() {
+    static int LastChar = ' ';
+
+    while (isspace(LastChar)){
+        LastChar = getchar();
+    }
+
+    if(isalpha(LastChar)){ //identifier: [a-zA-Z][a-zA-Z0-9]*
+        IdentifierStr = LastChar;
+        while (isalnum((LastChar = getchar()))){
+            IdentifierStr += LastChar;
+        }
+    }
+
+    if (IdentifierStr == "turn")
+        return tok_turn;
+    else if ((IdentifierStr == "u") || (IdentifierStr == "m1") || (IdentifierStr == "b") || (IdentifierStr == "m2") || (IdentifierStr == "e")){
+        return tok_phase;
+    }
+    else if ((IdentifierStr == "play") || (IdentifierStr == "attack") || (IdentifierStr == "pass")){
+        return tok_action;
+    }
+
+    //for now, there should be only integers in the program text code
+    if (isdigit(LastChar)){
+        std::string NumStr;
+        do {
+            NumStr += LastChar;
+            LastChar = getchar();
+        } while (isdigit(LastChar));
+
+        NumVal = std::stoi(NumStr.c_str(), 0);
+        return tok_number;
+    }
+
+    if (LastChar == '"'){
+        //comment for 1 line
+        //for now
+        do {
+            LastChar = getchar();
+        } while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+        if (LastChar != EOF){
+            return gettok();
+        }
+    }
+
+    if (LastChar == EOF){
+        return tok_eof;
+    }
+
+    int ThisChar = LastChar;
+    LastChar = getchar();
+    return ThisChar;
+}
---
+++ impl/parser.cpp
@@ -0,0 +1,113 @@
+#include <cstdio>
+#include <memory>
+#include <vector>
+
+/// CurTok/getNextToken - Provide a simple token buffer.  CurTok is the current
+/// token the parser is looking at.  getNextToken reads another token from the
+/// lexer and updates CurTok with its results.
+
+#include "../header/lexer.h"
+static int CurTok;
+static int getNextToken() {
+    return CurTok = gettok();
+}
+
+#include "../header/AST.h"
+/// LogError* - These are little helper functions for error handling.
+std::unique_ptr<ExprAST> LogError(const char *Str) {
+    fprintf(stderr, "LogError: %s\n", Str);
+    return nullptr;
+}
+
+/// numberexpr ::= number
+static std::unique_ptr<ExprAST> ParseNumberExpr() {
+    auto Result = std::make_unique<NumberExprAST>(NumVal);
+    getNextToken(); // consume the number
+    return std::move(Result);
+}
+
+/// identifier ::= identifier;
+static std::unique_ptr<ExprAST> ParseIdentifierExpr() {
+    std::string idName = IdentifierStr;
+
+    getNextToken();
+
+    return std::make_unique<IdentifierExprAST>(idName);
+}
+
+/// actionexpr ::= action
+/// actionexpr ::= action ((number)(identifier,))*(number)identifier(;)
+static std::unique_ptr<ExprAST> ParseActionExpr() {
+    std::string idName = IdentifierStr;
+
+    getNextToken();
+    
+    std::vector<std::unique_ptr<ExprAST>> args;
+    while(1) {
+        if(CurTok==tok_number){
+            auto arg = ParseNumberExpr();
+            args.push_back(std::move(arg));
+        }
+        else if(auto arg = ParseActionExpr()){
+            args.push_back(std::move(arg));
+        }
+        else
+            return nullptr;
+        
+        if((CurTok==tok_phase) || (CurTok==';'))
+            break;
+        
+        if(CurTok!=',')
+            return LogError("Expected a newline, ';', or ',' instead.");
+        
+        getNextToken();
+    }
+
+    return std::make_unique<ActionExprAST>(idName, std::move(args));
+}
+
+/// turnexpr ::= turn number
+static std::unique_ptr<ExprAST> ParseTurnExpr() {
+    getNextToken();
+    if(CurTok!=':')
+        return LogError("Exprected a ':' after a turn declaration.");
+    
+    getNextToken();
+    if(CurTok!=tok_number)
+        return LogError("Expected turn number.");
+    
+    auto Result = std::make_unique<TurnExprAST>(NumVal);
+    getNextToken(); // consume the number
+    return std::move(Result);
+}
+
+/// phaseexpr ::= phase (action;)*action
+static std::unique_ptr<ExprAST> ParsePhaseExpr() {
+    std::string idName = IdentifierStr;
+
+    getNextToken();
+
+    if (CurTok!=':')
+        return LogError("Expected ':' after a phase expression.");
+    
+    getNextToken();
+
+    std::vector<std::unique_ptr<ExprAST>> args;
+    while(1) {
+        if(auto arg = ParseActionExpr()){
+            args.push_back(std::move(arg));
+        }
+        else
+            return nullptr;
+        
+        if(CurTok==tok_phase) //newline
+            break;
+        
+        if(CurTok!=';')
+            return LogError("Expected a newline or ';' instead.");
+        
+        getNextToken();
+    }
+
+    return std::make_unique<PhaseExprAST>(idName, std::move(args));
+}
